cfdd399abf4f64c99767de69d76b562a36e8ab40
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-09 08:49:48 -0700

    Added dragon and dragon-test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.amountEaten = 0
+  }
 
+  greet(){
+    return `Hi, ${this.rider}!`;
+  }
+  eat(){
+    if(this.amountEaten < 3){
+      this.amountEaten += 1;
+      console.log(this.amountEaten);
+    }
+    if(this.amountEaten >= 3){
+      this.hungry = false;
+    }
+  }
+}
+
+
+
+module.exports = Dragon;
